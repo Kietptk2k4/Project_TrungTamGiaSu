0a933efc1ea4ac12872ef7153b1c5d7b3397a2cb
VŨ TIẾN ĐẠT <169063302+vutiendat24@users.noreply.github.com>
2025-04-13 11:38:44 +0700

    Update App.jsx

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,12 +1,6 @@
-import Header from "./components/Header/Header"
-import HeroSection from "./components/TutorTitle/TutorTitle"
-import CategoryNav from "./components/CategoryNav/CategoryNav"
-import TutorsList from "./components/TutorsList/TutorsList"
+
 import "./App.css"
-import CourseList from "./components/CourseList/CourseList"
 import AuthPage from "./components/Login/AuthPage"
-import TutorPage from "./pages/CustomerPage"
-import PaymentPage from "./pages/PaymentPage"
 import CustomerPage from "./pages/CustomerPage"
 import TutoringRequestForm from "./pages/TutoringRequestForm"
 
